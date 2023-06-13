249adc99487e3c972f8d83ce03302effb2d6bf31
riottecboi <riotecboi@gmail.com>
2023-06-13 14:40:59 +0700

    updated share template with CSS & logical functions

--- datamodel.py
+++ datamodel.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import time
-
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 class Datamodel:
@@ -41,38 +41,63 @@ class Datamodel:
             else:
                 return None
 
-        def __exit__(self):
+        def __exit__(self, exc_type, exc_value, tb):
             self.cursor.close()
             self.cnx.close()
 
-    def users(self, email):
+    def users(self, email, password):
         with self.MysqlConnector(self.sqlconfig, self.retries, self.waitTime) as (cursor, conn):
             try:
                 query = "SELECT * FROM users WHERE email = %s"
                 cursor.execute(query, (email,))
-                emailRes = cursor.fetchone()
-                if emailRes is not None:
-                    return emailRes[1]
-                query = "INSERT INTO users (email) VALUES (%s)"
-                cursor.execute(query, (email,))
+                userRes = cursor.fetchone()
+                if userRes is not None:
+                    isAuthenticated = check_password_hash(userRes[2], password)
+                    return userRes[1], isAuthenticated
+                current_time = datetime.now()
+                password_hash = generate_password_hash(password)
+                query = "INSERT INTO users (email, password_hash, updated) VALUES (%s,%s,%s)"
+                cursor.execute(query, (email,password_hash,current_time))
                 conn.commit()
-                return email
+                return email, True
 
             except Exception as e:
                 conn.rollback()
                 print(f"Exception occured: {str(e)}")
-                return ''
+                return '', False
 
-    def url_share(self, url):
+    def url_share(self, url, title, description, user):
         with self.MysqlConnector(self.sqlconfig, self.retries, self.waitTime) as (cursor, conn):
             try:
-                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                query = "INSERT IGNORE INTO urls (url, created_at) VALUES (%s, %s)"
-                cursor.execute(query, (url,current_time))
+                current_time = datetime.now()
+                query = "INSERT IGNORE INTO urls (url, title, description, updated_by, upload) VALUES (%s, %s, %s, %s, %s)"
+                cursor.execute(query, (url, title, description, user, current_time))
                 conn.commit()
+
                 return True
 
             except Exception as e:
                 conn.rollback()
                 print(f"Exception occured: {str(e)}")
                 return False
+
+    def get_video_shared(self):
+        with self.MysqlConnector(self.sqlconfig, self.retries, self.waitTime) as (cursor, conn):
+            results = []
+            try:
+                query = "SELECT * FROM urls"
+                cursor.execute(query)
+                urlRes = cursor.fetchall()
+                if urlRes is not None:
+                    for result in urlRes:
+                        url = result[1]
+                        if 'watch?v=' in url:
+                            url = url.split('watch?v=')[1].split('&')[0]
+                        if 'youtu.be' in url:
+                            url = url.split('youtu.be/')[1].split('?')[0]
+                        results.append({'url': result[1], 'title': result[2], 'description': result[3], 'user': result[4]})
+                return results
+            except Exception as e:
+                conn.rollback()
+                print(f"Exception occured: {str(e)}")
+                return results
--- main.py
+++ main.py
@@ -1,10 +1,72 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session
+from flask_env import MetaFlaskEnv
+from datamodel import Datamodel
+from flask_wtf import CSRFProtect
+
+
+class Configuration(metaclass=MetaFlaskEnv):
+    SECRET_KEY = "supersecretkey"
+    MYSQL_HOST = "xxx"
+    MYSQL_USER = "xxx"
+    MYSQL_PASSWORD = "xxx"
+    MYSQL_DB = "xxx"
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+try:
+    app.config.from_pyfile('settings.cfg')
+
+except FileNotFoundError:
+    # Load environment variables - docker/kubernetes integration
+    app.config.from_object(Configuration)
+
+sqlconfig = {
+    'host': app.config['MYSQL_HOST'],
+    'user': app.config['MYSQL_USER'],
+    'password': app.config['MYSQL_PASSWORD'],
+    'database': app.config['MYSQL_DB']
+}
+
+db = Datamodel(sqlconfig)
 
 @app.route('/')
-def home():
-    return render_template('main.html')
+def index():
+    return redirect(url_for('home'))
 
+@app.route('/home', methods=['POST','GET'])
+def home():
+    userLoggedin = False
+    shares = db.get_video_shared()
+    if 'email' in session:
+        userLoggedin = True
+        return render_template('main.html', userLoggedin=userLoggedin, email=session.get('email'), shares=shares)
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email, loggedin = db.users(username, password)
+        if not loggedin:
+            return redirect(url_for('home'))
+        session['email'] = email
+        userLoggedin = True
+        return render_template('main.html', userLoggedin=userLoggedin, email=session.get('email'), shares=shares)
+    return render_template('main.html',userLoggedin=userLoggedin, shares=shares)
+
+@app.route('/share', methods=['POST','GET'])
+def share():
+    if 'email' not in session:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        url = request.form.get('url')
+        title = request.form.get('title')
+        description = request.form.get('description')
+        share = db.url_share(url, title, description, session.get('email'))
+        return redirect(url_for('home'))
+
+    return render_template('share.html',userLoggedin=True, email=session.get('email'))
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run()
